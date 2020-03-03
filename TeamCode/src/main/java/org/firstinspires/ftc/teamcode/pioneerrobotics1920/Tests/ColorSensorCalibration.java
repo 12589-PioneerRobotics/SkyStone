c93edf45e90e5dd7bc98eaaa7ac5dac6b84624de
user12589 <12589.programmers@gmail.com>
2020-03-03 16:10:16 -0500

    imported odometery classes, wrote color sensor calibration, and PathGeneration test classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pioneerrobotics1920/Tests/ColorSensorCalibration.java
@@ -0,0 +1,124 @@
+package org.firstinspires.ftc.teamcode.pioneerrobotics1920.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.util.ReadWriteFile;
+
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+import org.firstinspires.ftc.teamcode.pioneerrobotics1920.Core.Driving;
+import org.firstinspires.ftc.teamcode.pioneerrobotics1920.Core.MoacV_2;
+import org.firstinspires.ftc.teamcode.pioneerrobotics1920.Core.Operations;
+import org.firstinspires.ftc.teamcode.pioneerrobotics1920.TeleOp.Toggle;
+
+import java.io.File;
+
+public class ColorSensorCalibration extends LinearOpMode {
+
+    Driving driving;
+    MoacV_2 moacV_2;
+    Toggle.OneShot hasStone;
+    Toggle.OneShot startWriting;
+    final double SCALE = 0.35;
+    boolean stone;
+    boolean writing;
+    File colorSensorCalibrationValue;
+    File emptyValues;
+    File stoneValues;
+    ColorSensor colorSensor;
+    String stoneInCollectedRaw;
+    String stoneOutCollectedRaw;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        driving = new Driving(this);
+        moacV_2 = new MoacV_2(hardwareMap);
+        hasStone = new Toggle.OneShot();
+        startWriting = new Toggle.OneShot();
+        stone = false;
+        writing = false;
+        colorSensorCalibrationValue = AppUtil.getInstance().getSettingsFile("colorSensorCalibrationValue.txt");
+        emptyValues = AppUtil.getInstance().getSettingsFile("emptyValues.txt");
+        stoneValues = AppUtil.getInstance().getSettingsFile("stoneValues.txt");
+        colorSensor = hardwareMap.colorSensor.get("brickSensor");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.left_bumper)
+                driving.libertyDrive(-Operations.powerScale(gamepad1.right_stick_y, SCALE), Operations.powerScale(gamepad1.right_stick_x + gamepad1.left_stick_x * -.35, SCALE), Operations.powerScale(gamepad1.left_stick_x, SCALE + 0.25));
+            else
+                driving.libertyDrive(-Operations.powerScale(gamepad1.right_stick_y), Operations.powerScale(gamepad1.right_stick_x + gamepad1.left_stick_x * -.3), gamepad1.left_stick_x);
+
+            if (gamepad1.left_trigger > .5) moacV_2.intake.spitOut();
+            else if (gamepad1.right_trigger > .5) moacV_2.intake.takeIn();
+            else moacV_2.intake.stopIntake();
+
+            if (hasStone.update(gamepad1.a))
+                stone = !stone;
+            if (startWriting.update(gamepad1.b))
+                writing = !writing;
+
+
+            if (writing) {
+                if (stone)
+                    ReadWriteFile.writeFile(stoneValues, getColorValues());
+                else
+                    ReadWriteFile.writeFile(emptyValues, getColorValues());
+            }
+
+            telemetry.addData("hasStone", stone);
+            telemetry.addData("writing", writing);
+            telemetry.update();
+        }
+
+        stoneInCollectedRaw = ReadWriteFile.readFile(stoneValues);
+        stoneOutCollectedRaw = ReadWriteFile.readFile(emptyValues);
+
+        String[] valueStringStoneIn = stoneInCollectedRaw.split("\n");
+        String[] valueStringStoneOut = stoneOutCollectedRaw.split("\n");
+
+        Point[] stoneInPoints = new Point[valueStringStoneIn.length];
+        Point[] stoneOutPoints = new Point[valueStringStoneOut.length];
+
+        String[] rgbValues;
+        for (int i = 0; i < stoneInPoints.length; i++) {
+            rgbValues = valueStringStoneIn[i].split(",");
+            stoneInPoints[i] = new Point(Integer.parseInt(rgbValues[0]), Integer.parseInt(rgbValues[1]), Integer.parseInt(rgbValues[2]));
+            rgbValues = valueStringStoneOut[i].split(",");
+            stoneOutPoints[i] = new Point(Integer.parseInt(rgbValues[0]), Integer.parseInt(rgbValues[1]), Integer.parseInt(rgbValues[2]));
+        }
+
+        ReadWriteFile.writeFile(colorSensorCalibrationValue, getCenter(stoneInPoints).toString() + "\n");
+        ReadWriteFile.writeFile(colorSensorCalibrationValue, getCenter(stoneOutPoints).toString() + "\n");
+    }
+
+    private String getColorValues() {
+        return colorSensor.red() + "," + colorSensor.green() + "," + colorSensor.blue() + "\n";
+    }
+
+    public Point getCenter(Point[] points) {
+        int sumX = 0, sumY = 0, sumZ = 0;
+        for (Point p : points) {
+            sumX += p.x;
+            sumY += p.y;
+            sumZ += p.z;
+        }
+        return new Point(sumX / points.length, sumY / points.length, sumZ / points.length);
+    }
+
+    class Point {
+        int x, y, z;
+
+        public Point(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public String toString() {
+            return "" + x + "," + y + "," + z;
+        }
+    }
+}
+
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pioneerrobotics1920/Tests/PathGeneration.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode.pioneerrobotics1920.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.pioneerrobotics1920.Core.Operations;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class PathGeneration extends OpMode {
+    DcMotor rf, rb, lf, lb;
+    final double SCALE = 0.35;
+    final String fileName = "pathGeneration";
+    BufferedWriter bw;
+
+    @Override
+    public void init() {
+
+        try {
+//            String content = "Separe here integers by semi-colon";
+            File file = new File(fileName + ".csv");
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+
+            bw = new BufferedWriter(fw);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        lf = hardwareMap.dcMotor.get("frontLeft");
+        rf = hardwareMap.dcMotor.get("frontRight");
+        lb = hardwareMap.dcMotor.get("backLeft");
+        rb = hardwareMap.dcMotor.get("backRight");
+
+        lf.setDirection(DcMotor.Direction.REVERSE);
+        rf.setDirection(DcMotor.Direction.FORWARD);
+        lb.setDirection(DcMotor.Direction.REVERSE);
+        rb.setDirection(DcMotor.Direction.FORWARD);
+
+        rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void libertyDrive(double drive, double turn, double strafe) {
+        double factor = Math.abs(drive) + Math.abs(turn) + Math.abs(strafe);
+        if (factor <= 1)
+            factor = 1;
+        lb.setPower((drive - strafe + turn) / factor);
+        lf.setPower((drive + strafe + turn) / factor);
+        rb.setPower((drive + strafe - turn) / factor);
+        rf.setPower((drive - strafe - turn) / factor);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.left_bumper)
+            libertyDrive(-Operations.powerScale(gamepad1.right_stick_y, SCALE), Operations.powerScale(gamepad1.right_stick_x + gamepad1.left_stick_x * -.35, SCALE), Operations.powerScale(gamepad1.left_stick_x, SCALE + 0.25));
+        else
+            libertyDrive(-Operations.powerScale(gamepad1.right_stick_y), Operations.powerScale(gamepad1.right_stick_x + gamepad1.left_stick_x * -.3), gamepad1.left_stick_x);
+
+        try {
+            bw.write("hello");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
